1eef027db6d49b713e42bf5ccba209eee63d2170
ddanciu2 <ddanci2@uic.edu>
2021-04-06 15:49:40 -0500

    Uh I forgot what I changed, went to sleep last night before pushing

--- RydeIndex.js
+++ RydeIndex.js
@@ -1,11 +1,11 @@
 /* Open when someone clicks on the span element */
 function openMenu() {
     document.getElementById("hamburgerMenu").style.zIndex = 0;
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("HMLayout").style.width = "100%";
   }
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
   function closeMenu() {
     document.getElementById("hamburgerMenu").style.zIndex = 1;
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("HMLayout").style.width = "0%";
   }
