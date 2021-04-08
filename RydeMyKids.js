915bfa6ca81ff4f68b8d5988327794b5c4b74a7c
ddanciu2 <ddanci2@uic.edu>
2021-04-08 02:08:15 -0500

    Added functionality to my kids

--- RydeMyKids.js
+++ RydeMyKids.js
@@ -9,3 +9,7 @@ function openMenu() {
     document.getElementById("hamburgerMenu").style.zIndex = 1;
     document.getElementById("HMLayout").style.width = "0%";
   }
+
+  function submitChild(){
+    window.location.href= "MyKids.html";
+  }
