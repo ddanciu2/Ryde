b00efcce37d1e6a6cff7db3c597b066e90e839dc
ShahmeerAhmed <sahme48@uic.edu>
2021-04-26 18:39:13 -0500

    other small changes

--- RydeMyKids.js
+++ RydeMyKids.js
@@ -19,19 +19,29 @@ function submitChild(){
 
 var i = 0;
 
+/* Duplicates a child element */
 function duplicateChild(){
   var original = document.getElementById('Child' + i);
-  var clone = original.cloneNode(true); // "deep" clone
-  clone.id = "Child" + ++i; // there can only be one element with an ID
-  /* clone.onclick = duplicate */; // event handlers are not cloned
+  var clone = original.cloneNode(true);
+  clone.id = "Child" + ++i;
   original.parentNode.appendChild(clone);
+}
 
+/* Removes a child element */
+function removeChild() {
+  console.log("In removeChild()");
+  var child = document.getElementById('Child' + i);
+  var original = document.getElementById('Child' + --i);
+  var result = original.parentNode.hasChildNodes();
+  
+  if (result)
+    alert("There is a child");
+  //original.parentNode.removeChild(child);
 }
 
 function submitChild1(){
   duplicateChild();
   document.getElementById("enterChildInfo").style.display = "none";
-  
 }
 
 function backInfo(){
